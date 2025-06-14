e8e58209db0d7a6ca26856ee09758417f32594ac
Tahir Amin <ag.adnan99@gmail.com>
2025-06-14 18:50:59 +0500

    Add basic security middleware (#6)

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { registerUser, loginUser, logoutUser, getUserDetails, forgotPassword, resetPassword, updatePassword, updateProfile, getAllUsers, getSingleUser, updateUserRole, deleteUser } = require('../controllers/userController');
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/user_actions/auth');
+const { validateSignUp } = require('../middlewares/validator');
+const loginLimiter = require('../middlewares/loginLimiter');
 
 const router = express.Router();
 
-router.route('/register').post(registerUser);
-router.route('/login').post(loginUser);
+router.route('/register').post(validateSignUp, registerUser);
+router.route('/login').post(loginLimiter, loginUser);
 router.route('/logout').get(logoutUser);
 
 router.route('/me').get(isAuthenticatedUser, getUserDetails);
@@ -25,3 +27,4 @@ router.route("/admin/user/:id")
     .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 module.exports = router;
+
